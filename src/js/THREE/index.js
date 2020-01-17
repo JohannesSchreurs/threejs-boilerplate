f495df459a4de3df0261da537066f75853672c34
JohannesSchreurs <>
2020-01-17 23:33:43 +0100

    add basic gui to the scene

--- src/js/THREE/index.js
+++ src/js/THREE/index.js
@@ -8,6 +8,19 @@ import {
   Mesh
 } from "three";
 import stats from "../stats/index";
+//instanced, or just create a new gui by importing from the node_modules
+import gui from "../gui/index";
+//Uncomment next line for your own gui
+// import dat from "dat.gui"
+// const gui = new dat.GUI();
+
+const guiControls = {
+  rotationSpeed: 0.01,
+  cameraPosition: 5
+};
+
+gui.add(guiControls, "rotationSpeed", 0, 0.1, 0.005);
+gui.add(guiControls, "cameraPosition", 0, 10, 1);
 
 const scene = new Scene();
 const camera = new PerspectiveCamera(
@@ -29,13 +42,12 @@ const material = new MeshBasicMaterial({ color: 0xffffff, wireframe: true });
 const cube = new Mesh(geometry, material);
 scene.add(cube);
 
-camera.position.z = 5;
-
 const animate = () => {
   stats.update();
 
-  cube.rotation.x += 0.01;
-  cube.rotation.y += 0.01;
+  camera.position.z = guiControls.cameraPosition;
+  cube.rotation.x += guiControls.rotationSpeed;
+  cube.rotation.y += guiControls.rotationSpeed;
 
   renderer.render(scene, camera);
 
