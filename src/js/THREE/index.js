3b4ee90f5b74b74462d3c4ebaedbc60930eaff12
JohannesSchreurs <>
2020-01-17 21:05:15 +0100

    update to es6

--- src/js/THREE/index.js
+++ src/js/THREE/index.js
@@ -1,28 +1,35 @@
 //The most basic setup for a Three scene.
-import * as THREE from "three";
+import {
+  Scene,
+  PerspectiveCamera,
+  WebGLRenderer,
+  BoxGeometry,
+  MeshBasicMaterial,
+  Mesh
+} from "three";
 
-var scene = new THREE.Scene();
-var camera = new THREE.PerspectiveCamera(
+const scene = new Scene();
+const camera = new PerspectiveCamera(
   75,
   window.innerWidth / window.innerHeight,
   0.1,
   1000
 );
 
-var renderer = new THREE.WebGLRenderer({
+const renderer = new WebGLRenderer({
   canvas: document.getElementById("threeRenderCanvas")
 });
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
-var geometry = new THREE.BoxGeometry(1, 1, 1);
-var material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-var cube = new THREE.Mesh(geometry, material);
+const geometry = new BoxGeometry(1, 1, 1);
+const material = new MeshBasicMaterial({ color: 0x00ff00 });
+const cube = new Mesh(geometry, material);
 scene.add(cube);
 
 camera.position.z = 5;
 
-var animate = function() {
+const animate = () => {
   requestAnimationFrame(animate);
 
   cube.rotation.x += 0.01;
