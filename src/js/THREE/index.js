e4f6903f2cc38124e074320f404960eadbdbbb13
JohannesSchreurs <>
2020-01-17 23:46:51 +0100

    split the code of the gui

--- src/js/THREE/index.js
+++ src/js/THREE/index.js
@@ -8,19 +8,8 @@ import {
   Mesh
 } from "three";
 import stats from "../stats/index";
-//instanced, or just create a new gui by importing from the node_modules
-import gui from "../gui/index";
-//Uncomment next line for your own gui
-// import dat from "dat.gui"
-// const gui = new dat.GUI();
 
-const guiControls = {
-  rotationSpeed: 0.01,
-  cameraPosition: 5
-};
-
-gui.add(guiControls, "rotationSpeed", 0, 0.1, 0.005);
-gui.add(guiControls, "cameraPosition", 0, 10, 1);
+import { settings } from "../gui/index";
 
 const scene = new Scene();
 const camera = new PerspectiveCamera(
@@ -45,9 +34,9 @@ scene.add(cube);
 const animate = () => {
   stats.update();
 
-  camera.position.z = guiControls.cameraPosition;
-  cube.rotation.x += guiControls.rotationSpeed;
-  cube.rotation.y += guiControls.rotationSpeed;
+  camera.position.z = settings.cameraPosition;
+  cube.rotation.x += settings.rotationSpeed;
+  cube.rotation.y += settings.rotationSpeed;
 
   renderer.render(scene, camera);
 
--- src/js/gui/index.js
+++ src/js/gui/index.js
@@ -2,4 +2,10 @@ import * as dat from "dat.gui";
 
 const gui = new dat.GUI();
 
-export default gui;
+export const settings = {
+  rotationSpeed: 0.01,
+  cameraPosition: 5
+};
+
+gui.add(settings, "rotationSpeed", 0, 0.1, 0.005);
+gui.add(settings, "cameraPosition", 0, 10, 1);
